38aa5faf2ff88bf1c83c57123bf1cb4e8d31f711
shaye <sm0548540902@gmail.com>
2025-11-10 15:54:56 +0200

    מוסיף את מחלקת book

---
+++ book.py
@@ -0,0 +1,33 @@
+class Book:
+    def __init__(self,name,title,author,isbn,is_available=True):
+        self.__name = name
+        self.__title = title
+        self.__author = author
+        self.__isbn = isbn
+        self.__is_available = is_available
+
+    @property
+    def is_available(self):
+        return self.__is_available
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def author(self):
+        return self.__author
+
+    @property
+    def isbn(self):
+        return self.__isbn
+
+    def set_is_available(self,type:bool):
+        self.__is_available = type
+
+    def __str__(self):
+        return f"the book is {self.__name} in title {self.__title} of {self.__author} the isbn is {self.__isbn} and this is available?:\n{self.__is_available}"
